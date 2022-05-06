45cfe6a762475983e679de81d468140e4ae34e60
Hank Jackson <hjackson277@gmail.com>
2022-05-06 10:21:51 -0400

    deleted extra unused code and comments

--- Contents.js
+++ Contents.js
@@ -1,26 +1,13 @@
-
-console.log('extention has loaded')
-
-// function show_prompt() {
-//   let name = prompt('Please enter your name', 'your name...');
-//   if (name != null && name != "") {
-//     alert(name);
-//   }
-// }
-
 const divs = document.getElementsByTagName('div')
 
-let degLevel = 1
-
-function degrade (num) {
-  let pos = 1;
+function degrade () {
   for (let i = 0; i < divs.length; i++){
     if (i % 2 === 0){
       divs[i].animate(
         [
-          { transform: `rotate(0) translate3D(-${.5}%, -${.5}%, 0)`, color: '#000' },
+          { transform: `rotate(0) translate3D(-.5%, -.5%, 0)`, color: '#000' },
           { color: '#431236', offset: 0.3 },
-          { transform: `rotate(${360}deg) translate3D(-${.5}%, -${.5}%, 0)`, color: '#000' }
+          { transform: `rotate(360deg) translate3D(-.5%, -.5%, 0)`, color: '#000' }
         ], {
           duration: 10000000,
           iterations: Infinity
@@ -29,34 +16,62 @@ function degrade (num) {
     else {
       divs[i].animate(
         [
-          { transform: `rotate(360deg) translate3D(${.5}%, ${.5}%, 0)`, color: '#000' },
+          { transform: `rotate(360deg) translate3D(.5%, .5%, 0)`, color: '#000' },
           { color: '#431236', offset: 0.3 },
-          { transform: `rotate(0) translate3D(${.5}%, ${.5}%, 0)`, color: '#000' }
+          { transform: `rotate(0) translate3D(.5%, .5%, 0)`, color: '#000' }
         ], {
           duration: 10005000,
           iterations: Infinity
         })
     }
-
   }
 }
 
-degrade(degLevel)
-
-function degradeBack (num) {
-  for (let i = 0; i < divs.length; i++){
-    divs[i].cancel()
+function degradeBack () {
+  const animations = document.getAnimations()
+  for (let i = 0; i < animations.length; i++){
+    animations[i].cancel()
   }
 }
 
-setTimeout(degradeBack(degLevel), 10000)
+degrade()
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const questions = [
+    {'In what year was the first iPhone released?': '2007'}, 
+    {'How many days are in the year?': '365'},
+    {'What does CPU stand for?': 'Central Processing Unit'},
+    {'What does WWW stand for?': 'World Wide Web'},
+    {'What HTML stand for?': 'Hypertext Markup Language'},
+    {'What is Will Sentance\'s favorite candy?': 'Twin Snakes'},
+    {'Who killed Han Solo?': 'Kylo Ren'}
+  ]
+
+  function getRandomQuestion (questions) {
+    let index = Math.floor((Math.random() * questions.length))
+    const question = document.querySelector('.question');
+    question.innerHTML = Object.keys(questions[index])[0];
+    return questions[index]
+  }
+
+  let currentQuestion = getRandomQuestion(questions)
+
+  const form = document.getElementById('buttonForQuestion')
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const answer = document.getElementById("answer_field").value
+    const answerKey = Object.keys(currentQuestion)[0];
+    if (currentQuestion[answerKey].toLowerCase() === answer.toLowerCase()){
+      currentQuestion = getRandomQuestion(questions);
+      chrome.tabs.executeScript(null, {code: 'degradeBack()'});
+    }
+    document.getElementById("answer_field").value = '';
+  }, false)
 
 
-
-
-function submitAnswer() {
-    alert("TESTTT'");
-}
+}, false)
 
 
 
